8b07e5c8a422c5b36e773bbdf4abe116d35037af
thoufiq135 <shaikno150@gmail.com>
2026-06-18 22:24:27 +0530

    first commit

--- src/adminDashboard.jsx
+++ src/adminDashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API_URL = "https://emp.stackenzo.com/api/fill/getAttendanceAdmin";
+const API_URL = "https://emp-backend.stackenzo.com/api/fill/getAttendanceAdmin";
 
 const FILTERS = [
   { label: "Today", value: "today" },
